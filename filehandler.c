2d9bcbd7844d732f0394a559b96178dee48415a9
siamn <siam.nafis@gmail.com>
2023-09-11 17:22:57 +0100

    added saving for teacher data (not loading)

--- filehandler.c
+++ filehandler.c
@@ -4,36 +4,42 @@
 #include "header.h"
 
 int saveStudent(FILE *file, Student *student);
+int saveTeacher(FILE *file, Teacher *teacher);
 
 int save(Student **students, int numOfStudents, Teacher **teachers, int numOfTeachers)
 {
-    FILE *file = fopen("students.csv", "w"); // open file in write mode (creates new file if it doesn't already exist)
-
-    if (file == NULL)
+    FILE *studentsFile = fopen("students.csv", "w"); // open file in write mode (creates new file if it doesn't already exist)
+    FILE *teachersFile = fopen("teachers.csv", "w");
+    if (studentsFile == NULL || teachersFile == NULL)
     {
-        perror("Unable to open the file\n");
+        perror("Unable to open a required file\n");
         return -1;
     }
 
     for (int i = 0; i < numOfStudents; i++)
     {
-        if (saveStudent(file, students[i]) == -1)
+        if (saveStudent(studentsFile, students[i]) == -1)
         {
             return -1;
         }
     }
 
+    for (int i = 0; i < numOfTeachers; i++)
+    {
+        if (saveTeacher(teachersFile, teachers[i]) == -1)
+        {
+            return -1;
+        }
+    }
+
+    fclose(studentsFile);
+    fclose(teachersFile);
     return 1;
 }
 
+// TODO: Maybe add check for return value of fprintf (same as in saveTeacher)
 int saveStudent(FILE *file, Student *student)
 {
-    if (file == NULL)
-    {
-        perror("Could not open file.\n");
-        return -1;
-    }
-
     fprintf(file, "\"%s\",%d,", student->stud_name, student->subject_count);
     for (int i = 0; i < student->subject_count; i++)
     {
@@ -51,4 +57,10 @@ int saveStudent(FILE *file, Student *student)
     fprintf(file, "\n");
 
     return 0;
+}
+
+int saveTeacher(FILE *file, Teacher *teacher)
+{
+    // fprintf returns a negative value if an error occurs
+    return fprintf(file, "\"%s\",\"%s\"\n", teacher->teacher_name, teacher->subject.subj_name);
 }
