81c904b4591e566456df62ef41fd67f7595f8788
siamn <siam.nafis@gmail.com>
2023-09-12 18:26:35 +0100

    converted userAddNewStudents to new method

--- main.c
+++ main.c
@@ -490,6 +490,41 @@ int userAddNewStudents(int totalStudents)
     return totalStudents;
 }
 
+int userAddNewStudents2(StudentsList *list)
+{
+    int numOfStudents = -1;
+
+    int response;
+    while (numOfStudents < 0)
+    {
+        printf("Please enter how many students you would like to register: \n");
+        numOfStudents = getPositiveInt();
+        if (numOfStudents == -1)
+        {
+            printf("Encountered an error. Try again.\n");
+        }
+    }
+
+    for (int i = 0; i < numOfStudents; i++)
+    {
+        // char *studentName = (char *)malloc(sizeof(char) * 20);
+        printf("Please enter each student name, pressing the Enter key for each: \n");
+        char *studentName = getLimitedLine(20);
+
+        if (studentExists2(list, studentName) >= 0)
+        {
+            printf("\nThe student '%s' exists in the system already!  Exiting back to main menu ... \n", studentName);
+            return 1;
+        }
+
+        addStudent2(list, studentName);
+
+        // userAddSubject(studentName);
+    }
+
+    return 1;
+}
+
 void userAddSubjectToExistingStudent(int numOfStudents)
 {
     char *studentName = (char *)malloc(sizeof(char) * 20);
@@ -777,6 +812,10 @@ int main(void)
     displayStudents2(list);
     addSubject2(list, "Siam", "Science", 100.00);
     displayStudents3(list);
+
+    userAddNewStudents2(list);
+    displayStudents3(list);
+
     // while (option != QUIT)
     // {
     //     displayMenuOptions();
